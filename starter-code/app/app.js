ae19614a1968292bc694b436221f31274a36d250
jugbeep <patrickbotkins@gmail.com>
2017-12-19 16:31:57 -0700

    booky mcbookface posted

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -11,11 +11,32 @@ function makeList(value, index, ar){
 
 }
 
-bookArr.forEach(function(){
-	$('#books').append('<li>'+bookArr+'</li>');
+bookArr.forEach(function(i){
+	$('#books').append('<li>'+i.author+'</li>');
+	$('#books').append('<li>'+i.title+'</li>');
+	$('#books').append('<img src='+i.image+'>');
+	$('#books').append('<li>'+i.releaseDate+'</li>');
 });
 
+
+
+$('form').submit(function(){
+	event.preventDefault();
+	let xtitle = $('#book-title').val();
+	let xauthor = $('#book-author').val();
+	let ximage = $('#book-image').val();
+	let xdate = $('#book-release-date').val();
+	$.post('https://den-super-crud.herokuapp.com/books',{ 
+		image: ximage,
+		author: xauthor,
+		title: xtitle,
+		releaseDate: xdate
+	});
+})
+
 });
 
-
-
+// title: 'Booky McBookface',
+// 		image: 'https://images-na.ssl-images-amazon.com/images/I/51wmbpnvlNL.jpg',
+// 		author: 'David K. Fuller',
+// 		releaseDate: 'April 27th, 2016',
